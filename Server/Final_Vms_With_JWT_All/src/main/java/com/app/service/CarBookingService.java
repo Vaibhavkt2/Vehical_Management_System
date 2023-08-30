16816a22f2878c3649cfed6605bc243f19a6ff0b
Vaibhav <vaibhavkt2@gmail.com>
2023-08-30 21:53:18 +0530

    All Updated With All Functionalities..

---
+++ Server/Final_Vms_With_JWT_All/src/main/java/com/app/service/CarBookingService.java
@@ -0,0 +1,18 @@
+package com.app.service;
+
+import com.app.dto.CarBookingDTO;
+import com.app.dto.CarBookingPaymentStatusDTO;
+import com.app.entity.CarBooking;
+
+import java.util.List;
+
+public interface CarBookingService {
+    CarBooking addBooking(CarBookingDTO bookingDTO);
+    List<CarBooking> getBookingsByUserId(Long userId);
+    List<CarBooking> getBookingsByCarId(Long carId);
+    List<CarBooking> getAllBookings();
+    CarBooking updateBooking(Long bookingId, CarBookingDTO bookingDTO);
+    void deleteBooking(Long bookingId);
+    CarBooking getBookingById(Long bookingId);
+    CarBooking updateStatus(Long bookingId, CarBookingPaymentStatusDTO payDTO);
+}
